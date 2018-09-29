35aeda9874da6ebe396a5575e90e0a46b7efc0cf
fengliao <your email>
2018-09-29 20:42:09 +0800

    zipkin 全链路追踪监测 服务调用

---
+++ zipkinserver/src/main/java/com/fengliao/ZipkinserverApplication.java
@@ -0,0 +1,14 @@
+package com.fengliao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinserverApplication.class, args);
+	}
+}
