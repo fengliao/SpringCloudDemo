7f69f99146edd5c7fb94b44c186e71c74761d5d7
fengliao <your email>
2018-10-02 15:30:36 +0800

    Hystrix Turbine 断路器聚合监控

--- eureka-client/src/main/java/com/fengliao/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/fengliao/EurekaClientApplication.java
@@ -1,15 +1,21 @@
 package com.fengliao;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 @RestController
 public class EurekaClientApplication {
 
@@ -21,7 +27,13 @@ public class EurekaClientApplication {
 	String port;
 
 	@RequestMapping("/hi")
+	@HystrixCommand(fallbackMethod = "helloError")
 	public String hello(@RequestParam(value = "name",defaultValue = "冯辽")String name){
 		return "hello"+name+",端口号:"+port;
 	}
+
+	public String helloError(String name){
+		return "sorry,server-error";
+	}
+
 }
